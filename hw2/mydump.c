e5f6660fb8543d69bc9e1c3801d5c3845e7cc391
Venkatakrishnan R <venkrish007@gmail.com>
2016-03-02 17:44:50 -0500

    getopt

---
+++ hw2/mydump.c
@@ -0,0 +1,47 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+	int opt = 0;
+	char *interface = NULL;
+	char *file = NULL;
+	char *str = NULL;
+	char *expr = NULL;
+
+	while ((opt = getopt(argc, argv, "i:r:s:")) != -1) {
+		switch(opt) {
+			case 'i':
+				interface = optarg;
+				//printf("interface: %s\n", interface);
+				break;
+			case 'r':
+				file = optarg;
+				//printf("file: %s\n", file);
+				break;
+			case 's':
+				str = optarg;
+				//printf("string: %s\n", str);
+				break;
+			default:
+				printf("Specify proper options\nUse mydump -h for Help\n");
+				return 0;
+		}
+	}
+	if(argc != 4) {
+		printf("Specify proper options\nUse mydump -h for Help\n");
+		return 0;
+	}
+	expr = argv[argc - 1];
+	printf("expr: %s\n", expr);
+}
